d3483730fac703a160ef3d4ba388d8b7f2fbbc6f
Abengkris <105534577+abengkris@users.noreply.github.com>
2024-04-17 17:41:49 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,16 +1,16 @@
 export default {
   // your domain name
-  host: "zaps.lol",
+  host: "indonostr.xyz",
 
   // set to true to disable registration site-wide
   registrationDisabled: false,
 
   // site slogan
-  slogan: "Free nostr addresses.",
+  slogan: "Indonesian Nostr People",
 
   // your donation LNURL
   donationLNUrl:
-    "LNURL1DP68GURN8GHJ7MR9VAJKUEPWD3HXY6T5WVHXXMMD9AKXUATJD3CZ7JMPVDV9YCC4FVREX",
+    "lnurl1dp68gurn8ghj7ctsdyh85etzv4jx2efwd9hj7a3s9aex2ut4v4ehgttnw3shg6tr943ksctjvajhxtmxx9jnzwf5xvcz6vphvyuz6dp3vvcj6wp3v56z6cf5v4skgdmy8q6xvdp4pwm3zy",
 
   // name of your MongoDB database
   dbName: "users",
