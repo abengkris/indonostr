3d8f3bf7b4ffc7b19237e7e9680b72b45a64c531
Abengkris <105534577+abengkris@users.noreply.github.com>
2024-04-18 02:33:04 +0700

    Update useCopyToClipboard.ts

--- src/hooks/useCopyToClipboard.ts
+++ src/hooks/useCopyToClipboard.ts
@@ -10,7 +10,7 @@ function useCopyToClipboard(): [CopiedValue, CopyFn] {
 
   const copy: CopyFn = async text => {
     if (!navigator?.clipboard) {
-      console.warn("Clipboard not supported");
+      console.warn("Papan klip tidak didukung");
       return false;
     }
 
@@ -20,7 +20,7 @@ function useCopyToClipboard(): [CopiedValue, CopyFn] {
       setCopiedText(text);
       return true;
     } catch (error) {
-      console.warn("Copy failed", error);
+      console.warn("Penyalinan gagal", error);
       setCopiedText(null);
       return false;
     }
